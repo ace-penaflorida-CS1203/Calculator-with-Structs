78772bfdb0650bb238b50c2ddfbb73f9667b2920
ace_peñaflorida <124572735+ace-penaflorida-CS1203@users.noreply.github.com>
2023-06-21 17:55:15 +0800

    Update Calculator Main.cpp

--- Calculator Main.cpp
+++ Calculator Main.cpp
@@ -3,6 +3,7 @@
 # include <cstdlib>
 # include <string>
 # include <typeinfo>
+# include <limits>
 
 using namespace std;
 
@@ -21,19 +22,41 @@ struct Values{
 
 
 double add(float num1, float num2) {
-  return num1 + num2;}
+  return num1 + num2;
+}
 
 double subtract(float num1, float num2) {
-  return num1 - num2;}
+  return num1 - num2;
+}
 
 double multiply(float num1, float num2) {
-  return num1 * num2;}
+  return num1 * num2;
+}
 
 double divide(float num1, float num2) {
-  return num1 / num2;}
+  return num1 / num2;
+}
 
 
-// resuable code for each operation: the sizeVal is passed as a reference so that the value will update for each iteration
+// 1.) Handling Errors
+bool isValidOperation(char operation) {
+    return operation == '+' || operation == '-' || operation == '*' || operation == '/';
+}
+
+bool isValidInput(int num1, char operation, int num2) {
+    if (cin && isValidOperation(operation)) {
+        return true;
+    } else {
+        system("cls");
+        cout << "Error: Input valid values and operator. \n\n\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
+    }
+}
+
+
+//  2.) resuable code for each operation: the sizeVal is passed as a reference so that the value will update for each iteration
 void array_solutions(int &sizeVal, Values& value, double result){ 
   if(sizeVal < MAX_VAL_HISTORY){
           value.arrayNum1[sizeVal] = value.num1;
@@ -44,8 +67,9 @@ void array_solutions(int &sizeVal, Values& value, double result){
         }
 }
 
-
+//  3.) outputting the history
 void history_main(const Values valueInfo, int sizeVal){ // const keyword indicates that the valueInfo object cannot be modified within the function.
+  system("cls");
   string outputArray[MAX_VAL_HISTORY]; // with index 100
   cout << "\n|   HISTORY  |   \n"; 
   for (int i = 0; i < sizeVal; i++){ 
@@ -60,60 +84,119 @@ void history_main(const Values valueInfo, int sizeVal){ // const keyword indicat
 }
 
 
+// 4.) function if the user wants to perform another operation to the previous result
+void continueOperation (double prevResult){
+  double newValue;
+  double localResult = prevResult;
+  char newOperation;
+
+  double loopValues = localResult;
+  cout << "\n   New Operation: " << loopValues; 
+  cin >> newOperation >> newValue;
+
+  switch(newOperation){
+    case '+':
+      localResult = add(loopValues,newValue);
+      cout << "   New Result: " << loopValues << " + " << newValue << " = " << localResult << endl;
+      break;
+    case '-':
+      localResult = subtract(loopValues,newValue);
+      cout << "   New Result: " << loopValues << " - " << newValue << " = " << localResult << endl;
+      break;
+    case '*':
+      localResult = multiply(loopValues,newValue);
+      cout << "   New Result: " << loopValues << " * " << newValue << " = " << localResult << endl;
+      break;
+    case '/':
+      localResult = divide(loopValues,newValue);
+      cout << "   New Result: " << loopValues << " / " << newValue << " = " << localResult << endl;
+      break;
+  }
+}
+
+// 5.) resuable function for continuing the operation
+void userContinueChoice(double method) {
+    char userInput;
+    bool shouldContinue = true;
+    while (shouldContinue) {
+        cout << "\nCONTINUE OPERATION: |Y/N|";
+        cin >> userInput;
+        if (userInput == 'y' || userInput == 'Y') {
+            continueOperation(method);
+        } else if (userInput == 'n' || userInput == 'N') {
+            shouldContinue = false;
+        }
+    }
+}
+
+
 int main() {
 
     Values value;
     int sizeVal = 0;
+    string input;
+    bool isValid = false;
 
     do{
+      system("cls");
+      
+      while (!isValid) {
+        cout << "_____Mini Calculator_____\n\n";
+        cout << "   Enter Equation: ";
+        cin >> value.num1 >> value.operator1 >> value.num2;
 
-      cout << "\n\nNo Spaces\n";
-      cout << "\n   Values: ";
-      cin >> value.num1 >> value.operator1 >> value.num2;
+        isValid = isValidInput(value.num1, value.operator1, value.num2); // returns a bool 
+        }
+      
       
       switch(value.operator1){
 
         case '+':
           value.sum = add(value.num1, value.num2);
-          cout << "Sum: " << value.num1 << " + " << value.num2 << " = " << value.sum << endl;
+          cout << "\n   Sum: " << value.num1 << " + " << value.num2 << " = " << value.sum << endl;
 
-          array_solutions(sizeVal, value, value.sum);
-          history_main(value, sizeVal);
+          array_solutions(sizeVal, value, value.sum); // function for storing the values into the arrays
+          userContinueChoice(value.sum); // function if the user wanted to perform new operation to the recent result
+          history_main(value, sizeVal); // function for  history
           break;
 
 
         case '-':
           value.difference = subtract(value.num1, value.num2);
-          cout << "Difference: " << value.num1 << " - " << value.num2 << " = " << value.difference << endl;
+          cout << "\n   Difference: " << value.num1 << " - " << value.num2 << " = " << value.difference << endl;
 
           array_solutions(sizeVal, value, value.difference);
+          userContinueChoice(value.difference);
           history_main(value, sizeVal);
           break;
 
 
         case '*':
           value.product = multiply(value.num1, value.num2);
-          cout << "Product: " << value.num1 << " * " << value.num2 << " = " << value.product << endl;
+          cout << "\n   Product: " << value.num1 << " * " << value.num2 << " = " << value.product << endl;
 
           array_solutions(sizeVal, value, value.product);
+          userContinueChoice(value.product);
           history_main(value, sizeVal);
           break;
 
 
         case '/':
           value.quotient = divide(value.num1, value.num2);
-          cout << "Quotient: " << value.num1 << " +/ " << value.num2 << " = " << value.quotient << endl;
+          cout << "\n   Quotient: " << value.num1 << " +/ " << value.num2 << " = " << value.quotient << endl;
 
           array_solutions(sizeVal, value, value.quotient);
+          userContinueChoice(value.quotient);
           history_main(value, sizeVal);
           break;
 
         default:
-          cout << "Invalid Operation";
+          cout << "\n   Invalid Operation";
       }
 
-      cout << "\nCONTINUE: |Y/N|";
+      cout << "\n   CONTINUE: |Y/N|";
       cin >> backChar;
+      isValid = false; // resetting the variable 
 
     }while(backChar == 'y' || backChar == 'Y');
    
